f4c85aa65d32f0da4377fcc5e710483531e4f3d5
ben_kung <ben_kung@KHSE0-L2007001A>
2026-06-16 13:31:37 +0800

    feat: 新增 API 代理設定至 Vite 配置

--- neo-frontend/vite.config.ts
+++ neo-frontend/vite.config.ts
@@ -22,6 +22,12 @@ export default defineConfig({
     port: 3000,
     open: true,
     host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5058',
+        changeOrigin: true,
+      },
+    },
   },
   resolve: {
     alias: {
